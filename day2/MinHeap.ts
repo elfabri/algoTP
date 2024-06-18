ee64e7cc765b0f5250e9c15d3935ad2cf21a64d2
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-06-18 20:42:58 -0300

    MinHeap with array

---
+++ day2/MinHeap.ts
@@ -0,0 +1,88 @@
+export default class MinHeap {
+    // Binary tree where every child and grand-child
+    // is larger than the current node.
+    // Weak order.
+    // Implemented on an array,
+    // could be implemented on an array list.
+    // Could implement an update method.
+    public length: number;
+    private data: number[];
+
+    constructor() {
+        this.length = 0;
+        this.data = [];
+
+    }
+
+    insert(value: number): void {
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
+
+    delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
+
+        const out = this.data[0];
+        if (this.length === 1) {
+            this.length--;
+            this.data = [];
+            return out;
+        }
+
+        this.length--;
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+
+    private rightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
+
+    private heapifyUp(idx: number): void {
+        if (idx === 0) return;
+
+        const p = this.parent(idx);
+        const parentV = this.data[p];
+        const v = this.data[idx];
+
+        if (parentV > v) {
+            this.data[idx] = parentV;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
+
+    private heapifyDown(idx: number): void {
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (idx >= this.length || lIdx >= this.length) return;
+
+        const lV = this.data[lIdx];
+        const rV = this.data[rIdx];
+        const v = this.data[idx];
+
+        if (lV > rV && v > rV) {
+            this.data[idx] = rV;
+            this.data[rIdx] = v;
+            this.heapifyDown(rIdx);
+
+        } else if (rV > lV && v > lV) {
+            this.data[idx] = lV;
+            this.data[lIdx] = v;
+            this.heapifyDown(lIdx);
+        }
+    }
+}
